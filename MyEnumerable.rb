ebe04651365465a24f121e9d10a8029063e755fc
Reem <janinareem@gmail.com>
2022-04-01 00:16:28 +0200

    Test the any function

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,11 +1,32 @@
 module MyEnumerable
-  def all? #A
+  def all? 
     state = true
     unless block_given?
-      each { |obj| state = false unless obj}
-      return state 
+      each { |obj| state = false unless obj }
+      return state
     end
-    each { |n| state=false unless yield n}
+    each { |n| state = false unless yield n }
     state
   end
-end 
+
+  def any?
+    state = false
+    unless block_given?
+      each { |item| state = true if item}
+      return state
+    end
+    each { |n| state = true if yield n}
+    state
+  end
+
+  def filter?
+    state = []
+    unless block_given?
+      each { |n| state.push(n) }
+      return state
+    end
+    each { |x| state.push(x) if yield x }
+  end
+end
+
+
